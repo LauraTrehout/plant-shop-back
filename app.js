4314a55e8430b9e7c221af67d02e282c77fd83c1
LauraTrehout <lauratrehout@gmail.com>
2021-10-27 19:04:54 +0200

    connection to mongodb and crud on items

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const connection = require('./config/db-config')
-const { setupRoutes } = require('./routes')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const app = express();
+
 
 const port = process.env.PORT || 4000
 
+//Middleware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('/public'))
 
 //Connection to db
-connection.connect(err => {
-    if (err) {
-      console.error('error connecting: ' + err.stack)
-    } else {
-      console.log('connected to database with threadId :  ' + connection.threadId)
-    }
+mongoose.connect("mongodb://localhost/plants", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Define model
+const Plant = mongoose.model(
+  "plant",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    name: String,
+    price: Number,
+    image: String,
+    category: String,
   })
+);
 
-  //Middleware
-  app.use(cors())
-  app.use(morgan('tiny'))
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.static('/public'))
 
-  setupRoutes(app)
+//Routes
+app.get("/plants", async (req, res) => {
+  const plant = await Plant.find({})
+  res.send(plant)
+});
 
-  app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-  })
+app.post("/plants", async (req, res) => {
+  const newPlant = new Plant(req.body);
+  const savedPlant = await newPlant.save()
+  res.send(savedPlant)
+})
+
+  app.delete("/plants/:id", async (req, res) => {
+    const deletedPlant = await Plant.findByIdAndDelete(req.params.id)
+    res.send(deletedPlant)
+})
+
+//Listen port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
